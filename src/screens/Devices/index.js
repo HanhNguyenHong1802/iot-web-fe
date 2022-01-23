2e25d36083865f1207f145fc48e0fe47aab45787
phamducdat <phamducdat2402@gmail.com>
2022-01-23 19:43:58 +0700

    add sun

--- src/screens/Devices/index.js
+++ src/screens/Devices/index.js
@@ -9,11 +9,18 @@ import { DevicesContextProvider, useDevicesContext } from './context'
 const DevicesImpl = () => {
   const { listDevices, setOpenAddModal, openAddModal } = useDevicesContext()
 
-  const CardContainer = tw.div`flex flex-wrap w-full place-content-center`
+  const CardContainer = tw.div`flex flex-wrap w-full place-content-center z-50`
   const AddButton = tw.button`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg border-none float-right mr-5 cursor-pointer`
   return (
+
     <>
-      <NavTop />
+        <img className="sun item-1"
+             src="https://cdn2.iconfinder.com/data/icons/weather-emoticon/64/07_sun_smile_happy_emoticon_weather_smiley-512.png"></img>
+        <img className="sun item-2"
+             src="https://cdn2.iconfinder.com/data/icons/weather-emoticon/64/07_sun_smile_happy_emoticon_weather_smiley-512.png"></img>
+        <img className="sun item-3"
+             src="https://cdn2.iconfinder.com/data/icons/weather-emoticon/64/07_sun_smile_happy_emoticon_weather_smiley-512.png"></img>
+        <NavTop />
       <h1 style={{ marginTop: '8rem' }}>Devices Management</h1>
       <AddButton onClick={() => setOpenAddModal(true)}>+ Add Device</AddButton>
       <CardContainer>
